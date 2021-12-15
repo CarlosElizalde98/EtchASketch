8f2244f30d36786f1c1c0d59d1fc5d022d32caae
Carlos E <elizaldc@protonmail.com>
2021-12-15 09:18:07 -0600

    Added new buttons and functions to make selecting etch-a-sketch modes easier.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const container = document.querySelector('#container');
 const resetBtn = document.querySelector('#reset');
+const eraserBtn = document.querySelector('#eraser');
+const colorBtn = document.querySelector('#color');
 
-function createSquares() {
-    for (let j = 0; j < 16; j++) {
+//Function that will create the grid.
+function createSquares(number) {
+    for (let j = 0; j < number; j++) {
         const row = document.createElement('div');
         row.classList.add('row');
         container.appendChild(row);
-            for (let i = 0; i < 16;i++) {
+            for (let i = 0; i < number;i++) {
                 const item = document.createElement('div');
                 item.classList.add('item');
                 row.appendChild(item);
@@ -14,18 +17,34 @@ function createSquares() {
         }
 }
 
-createSquares();
-container.addEventListener("mouseover", function(event) {
-    event.target.style.backgroundColor = "black";
-    
-});
+createSquares(16);
 
-const gridItems = document.querySelectorAll('.item');
+colorBtn.addEventListener('click', colorItem);
+
 resetBtn.addEventListener('click', eraseGrid);
 
+eraserBtn.addEventListener('click', eraseItem);
 
+const gridItems = document.querySelectorAll('.item');
+
+//Event listener that will allow user to interact with grid.
+function colorItem() {
+
+    container.addEventListener("mouseover", function(event) {
+        event.target.style.backgroundColor = "black";
+        
+    }); 
+}
+
+//Function resets the grid back to default color.
 function eraseGrid() {
     gridItems.forEach(element => {
         element.style.backgroundColor = 'white';
+    });
+}
+
+function eraseItem() {
+    container.addEventListener('mouseover', function(event) {
+        event.target.style.backgroundColor = 'white';
     });
 }
